17776497ad759ce5cc1b67c2b44cbe75e6c0c929
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2023-05-27 18:05:39 -0300

    workin ok listo para entrega

--- mn-tp2-jacobi.py
+++ mn-tp2-jacobi.py
@@ -1,6 +1,5 @@
 import math
 import numpy as np
-from scipy.linalg import solve
 
 
 # Funciones auxiliares
@@ -13,13 +12,22 @@ def ingresar_entero(mensaje):
             print("Error: Debes ingresar un número entero válido.")
 
 
+def ingresar_decimal(mensaje):
+    while True:
+        try:
+            valor = float(input(mensaje))
+            return valor
+        except ValueError:
+            print("Error: Debes ingresar un número decimal válido.")
+
+
 def seleccionar_ingreso_data():
     o = 0
     while not 0 < o < 4:
         o = ingresar_entero("Ingrese:\n"
                             "1: para llenar la matriz con valores aleatorios\n"
                             "2: para ingresar de manera manual\n"
-                            "3: para trabajar con datos preestablecidos\n"
+                            "3: para trabajar con una matriz predefinida de 3x3 y un vector de n=3\n"
                             "Seleccione una opcion: ")
     return o
 
@@ -34,18 +42,18 @@ def verif_diagonal_dominante(M):
 
 
 def ingreso_datos_aleatorio(n):
-    i_maxima = 500
+    i_maxima = 1000
 
     print()
     print("Selecciono el modo de ingreso con valores aleatorios")
     print(f"Para llenar la matriz de {n} x {n} elementos para iterar en el método Jacobi")
-    print(f"se intentará, en lo posible, generar una matriz con diagonal dominante en {i_maxima} intentos.")
+    print(f"se generará, en lo posible, una matriz con diagonal dominante en {i_maxima} intentos como máximo.")
     print()
 
     MIN_VALUE = -5
     MAX_VALUE = 5
     print(f"Valores mínimos y máximos seteados por defecto en {MIN_VALUE} y {MAX_VALUE} respectivamente")
-    if input("Ingrese 'ENTER' para mantenerlos o 'r' para redefinir").upper() == "R":
+    if input("Ingrese 'ENTER' para mantenerlos o 'R' para redefinir").upper() == "R":
         MIN_VALUE = ingresar_entero("Ingrese un valor entero para el valor MÍNIMO del generador aleatorio")
         MAX_VALUE = ingresar_entero("Ingrese un valor entero para el valor MÁXIMO del generador aleatorio")
 
@@ -54,7 +62,7 @@ def ingreso_datos_aleatorio(n):
     while not es_diagonal_dominante and i < i_maxima:
         A = np.random.randint(MIN_VALUE, MAX_VALUE, size=(n, n))
         b = np.random.randint(MIN_VALUE, MAX_VALUE, size=n)
-        A = np.where(A == 0, 1, A)
+        A = np.where(A == 0, MIN_VALUE, A)
         es_diagonal_dominante = verif_diagonal_dominante(A)
         i += 1
         if i < i_maxima:
@@ -68,17 +76,35 @@ def ingreso_datos_aleatorio(n):
 
 
 def ingreso_datos_manual(n):
-    A = np.zeros((n, n))
+
+    print("Selecciono el modo de ingreso manual")
+
+    ingreso = "N"
+
+    while ingreso == "N":
+        A = np.zeros((n, n))
+        print("Ingreso de elementos de la matriz A:")
+        print("Nota: no se permite ingresar ningún valor en 0")
+        for i in range(n):
+            for j in range(n):
+                while A[i, j] == 0:
+                    A[i, j] = ingresar_decimal(f"Ingrese el elemento de la matriz: [fila {i + 1} / columna {j + 1}]: ")
+                    if A[i, j] == 0:
+                        print("DEBE INGRESAR UN VALOR DISTINTO DE CERO!\nIntente nuevamente!")
+
+        ingreso = ""
+        if not verif_diagonal_dominante(A):
+            print("\n*** Atención: la matriz ingresada no contiene una diagonal dominante ***")
+            print("*** Es posible que el método Jacobi no converga en un resultado ***\n")
+            print("Presiones ENTER para continuar o 'N' para ingresar nuevamente")
+            ingreso = str(input()).upper()
+        else:
+            print("Bien! La matriz ingresada tiene diagonal dominante, es ideal para iterar en Jacobi")
+
     b = np.zeros(n)
-
-    print("Ingreso de elementos de la matriz A:")
-    for i in range(n):
-        for j in range(n):
-            A[i, j] = float(input(f"Ingrese el elemento A[{i + 1}, {j + 1}]: "))
-
     print("\nIngreso de elementos del vector b:")
     for i in range(n):
-        b[i] = float(input(f"Ingrese el elemento b[{i + 1}]: "))
+        b[i] = ingresar_decimal(f"Ingrese el valor {i + 1} para el vector b: ")
 
     return A, b
 
@@ -163,38 +189,39 @@ def jacobi(A, b, k, tol):
 # message -> el mensaje devuelto por la funcion jacobi()
 # tol -> el valore de tolerancia o error abs seteado para calcular en cuantos decimales se formatearan los valores
 # e -> la solucion exacta para mostrar
-# n -> la dimension ingresada para los ciclos for para emitir los vectores
-def mostrar_resultados(H, norm, v, message, tol, e, n):
-    redondeo_decimales = abs(int(math.log10(tol))) + 1
+def mostrar_resultados(H, norm, v, message, tol, A, b):
+    redondeo_decimales = abs(int(math.log10(tol)))
+    n = len(v)
+    e = np.linalg.solve(A, b)
     print("================================ Emision de Resultados ===================================")
 
-    print("\n* Vectores resultados obtenidos *")
+    print("\n\t\t\t* * * * * * Vectores resultado obtenidos * * * * * *\t\t\t")
+    print("Aproximación de las soluciones")
     for i in range(len(norm)):
         # print("--------------------------------------------------------------------------------")
-        print(f"Aproximación de las soluciones en iteración {i + 1}:")
+        print(f"Iteración {i + 1}")
         for j in range(n):
             print(f" x{j + 1} = {np.round(H[i][j], redondeo_decimales)}", end="\t")
         if i != 0:
             print("\nNorma: ", np.round(norm[i], redondeo_decimales), end="\t")
         print("\n--------------------------------------------------------------------------------")
     print()
-    print("* Valores aproximados de las incógnitas obtenidas por método de Jacobi *")
+    print("* * * * * * Valores aproximados de las incógnitas obtenidas por método de Jacobi * * * * * *")
     print(message)
-    print(f"\nCon un error absoluto menor a {np.round(norm[-1], redondeo_decimales + 1)}")
-    print("El vector de resultados es:")
+    print("\nEl vector de resultados es:")
     for i in range(n):
         print(f" x{i + 1} = {np.round(v[i], redondeo_decimales)}", end="\t")
-    print("\n--------------------------------------------------------------------------------")
-    print("Solucion exacta con solve():")
+    print("\nSolucion exacta con solve():")
     for i in range(n):
         print(f" x{i + 1} = {np.round(e[i], redondeo_decimales)}", end="\t")
-    print("\n--------------------------------------------------------------------------------")
-    print("Diferencia:")
+    dif_prom = 0
     for i in range(n):
-        print(f" x{i + 1} = {np.round(e[i] - v[i], redondeo_decimales)}", end="\t")
-    print()
-
-    print("********************** Proceso de emisión de resultados finalizado ************************")
+        dif_prom += e[i] - v[i]
+    if dif_prom == float(0):
+        print("\n\nEl resultado de iterar con Jacobi coincide con la solución exacta con solve()")
+    else:
+        print("\n\nDiferencia promedio: ", dif_prom / n)
+    print("\n********************** Proceso de emisión de resultados finalizado ************************")
     print("===========================================================================================")
 
 
@@ -212,29 +239,31 @@ again = True
 
 while again:
     n = 0
+
     option = seleccionar_ingreso_data()
     print()
 
-    if option == 1 or option == 2:
+    if 0 < option <= 2:
         while n < 1:
-            n = ingresar_entero("Ingrese una dimension (n) para la matriz de (n x n):")
+            n = ingresar_entero("Ingrese una dimension (n) para la matriz de (n x n) para trabajar:")
         A = np.zeros((n, n))
         b = np.zeros(n)
+
         if option == 1:
             A, b = ingreso_datos_aleatorio(n)
 
         elif option == 2:
-            print("Selecciono el modo Manual")
             A, b = ingreso_datos_manual(n)
 
     else:
-        print("Selecciono trabajar con una matriz preestablecida")
+        print("Selecciono trabajar con una matriz preestablecida de 3 x 3")
         A, b = ingreso_datos_fijo()
         n = len(b)
 
     set_tol = -1
-    print(f"Ingrese la opción para el valor de precisión deseado")
+    print(f"Ingrese el valor de precisión de decimales deseados 0...9")
     print("Nota: a mayor precisión se requerirán mayor cantidad de iteraciones")
+    print("Ejemplos:")
     print("0 -> 0 (Precisión Baja)")
     print("1 -> 0.1")
     print("2 -> 0.01")
@@ -251,12 +280,11 @@ while again:
     while k <= 0:
         k = ingresar_entero("Ingrese el número de iteraciones (k): ")
 
-    H, norm, v, message = jacobi(A, b, k, tol)  # Aplicar el método de Jacobi
-    solve_solution = solve(A, b)
-    mostrar_resultados(H, norm, v, message, tol, solve_solution, n)
+    H, norm, v, message = jacobi(A, b, k, tol)  # Aplica el método de Jacobi con los datos ingresados
+    mostrar_resultados(H, norm, v, message, tol, A, b)
 
     print()
-    opc = str(input("Ingrese 'ENTER' para volver a empezar o 'x' para salir del programa").upper())
+    opc = str(input("Ingrese 'ENTER' para volver a empezar o 'X' para salir del programa").upper())
     if opc == "X":
         again = False
 
