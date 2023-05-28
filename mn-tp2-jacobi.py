c04efd913ce24cc2ad6b372f2a3598d75e040a4d
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2023-05-28 13:38:27 -0300

    comentarios y textos mod

--- mn-tp2-jacobi.py
+++ mn-tp2-jacobi.py
@@ -1,6 +1,53 @@
 import numpy as np
 
 
+# ********************************************************
+# * Universidad Nacional de San Martin                   *
+# * Escuela de Ciencia y Tecnologia                      *
+# * Trabajo Practico 2 - Métodos Numericos               *
+# * Alumno: Nava, Alejandro Daniel                       *
+# * DNI: 32956059                                        *
+# * Carrera: TPI                                         *
+# ********************************************************
+
+"""
+1. El programa comienza mostrando un menú inicial en el que el usuario puede elegir entre tres opciones para ingresar los datos del sistema de ecuaciones:
+
+   - Llenar la matriz con valores aleatorios.
+   - Ingresar manualmente la matriz y el vector de términos independientes.
+   - Trabajar con una matriz predefinida de 3x3 y un vector de n=3.
+
+2. Después de seleccionar la opción de ingreso de datos, se solicita al usuario ingresar la tolerancia deseada. La tolerancia determina la precisión de los decimales en el resultado y afecta la cantidad de iteraciones necesarias para converger a una solución.
+
+3. A continuación, se le pide al usuario ingresar el número máximo de iteraciones permitidas.
+
+4. Dependiendo de la opción seleccionada, el programa generará una matriz con valores aleatorios, solicitará al usuario que ingrese manualmente la matriz y el vector de términos independientes, o utilizará una matriz predefinida.
+
+5. Antes de comenzar las iteraciones del método de Jacobi, se muestran los datos iniciales del sistema de ecuaciones, incluyendo:
+   - La matriz ingresada.
+   - El vector de términos independientes.
+   - La tolerancia establecida.
+
+6. A continuación, se ejecuta el método de Jacobi para estimar las soluciones del sistema de ecuaciones. Se realiza un bucle iterativo hasta que se cumpla alguna de las siguientes condiciones:
+   - Se alcanza la cantidad máxima de iteraciones especificada.
+   - La diferencia entre dos iteraciones consecutivas es menor que la tolerancia establecida.
+
+7. Después de las iteraciones, se muestran los resultados obtenidos. Esto incluye:
+   - Los vectores resultado obtenidos en cada iteración.
+   - La norma correspondiente a cada iteración.
+   - El vector solución final obtenido por el método de Jacobi.
+   - El vector solución exacta calculado utilizando el método `np.linalg.solve()` de NumPy.
+
+8. El programa también verifica si la diferencia entre el vector solución exacta y el vector solución obtenido por el método de Jacobi es igual a cero. Si es así, se muestra un mensaje indicando que los resultados coinciden.
+
+9. Después de mostrar los resultados, se ofrece al usuario la opción de ejecutar el programa nuevamente o salir.
+
+El programa realiza algunas validaciones, como:
+   - Verificar si la matriz es singular (determinante igual a cero).
+   - Verificar si la matriz tiene una diagonal dominante.
+   - Permitir al usuario definir los valores mínimo y máximo para la generación de valores aleatorios en la matriz.
+"""
+
 # Funciones auxiliares
 def ingresar_entero(mensaje):
     while True:
@@ -158,9 +205,6 @@ def ingreso_datos_fijo():
 
 def jacobi(A, b, k, tol):
     # Imprimir los datos iniciales del sistema de ecuaciones
-    print("===========================================================================================")
-    print("-------- Método de Jacobi - Solución de un sistema de ecuaciones lineales -----------------")
-    print("-------------------------------------------------------------------------------------------")
     print("Matriz (A) ingresada:")
     print(A, "\n")
     print("Vector de términos independientes (b) ingresado:")
@@ -252,8 +296,9 @@ def mostrar_resultados(H, norm, v, message, tol, A, b):
         print("\n\nEl resultado de iterar con Jacobi coincide con la solución exacta con solve()")
     else:
         print("\n\nDiferencia promedio: ", dif_prom / n)
-    print("\n********************** Proceso de emisión de resultados finalizado ************************")
-    print("===========================================================================================")
+    print()
+    print("-------------------- Proceso de emisión de resultados finalizado --------------------")
+    print("-------------------------------------------------------------------------------------")
 
 
 # Menu del programa
@@ -285,14 +330,13 @@ def menu():
     mostrar_resultados(H, norm, v, message, tol, A, b)
 
 
-print("********************************************************")
-print("* Universidad Nacional de San Martin                   *")
-print("* Escuela de Ciencia y Tecnologia                      *")
-print("* Trabajo Practico 2 - Métodos Numericos               *")
-print("* Alumno: Nava, Alejandro Daniel                       *")
-print("* DNI: 32956059                                        *")
-print("* Carrera: TPI                                         *")
-print("********************************************************")
+print("==================================================================================================")
+print("======================       Trabajo Practico No.2 Metodos Numericos      ========================")
+print("======================       Alumno: Nava, Alejandro Daniel               ========================")
+print("==================================================================================================")
+print("--------------------------------------------------------------------------------------------------")
+print("----------------- Método de Jacobi - Solución de un sistema de ecuaciones lineales ---------------")
+print("--------------------------------------------------------------------------------------------------")
 
 print("\nComenzamos!\n")
 
@@ -305,5 +349,4 @@ while again:
 
 print("********************** Trabajo Practico No.2 Metodos Numericos Finalizado ************************")
 print("==================================================================================================")
-print("Muchas gracias por utilizar!")
-print("Atte. Alejandro Daniel Nava")
+print("\nMuchas gracias por utilizar!")
